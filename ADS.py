abd09d541b89a3aedc9b83e93e0ab48e05e39adb
09Bhakti <132741211+09Bhakti@users.noreply.github.com>
2025-04-03 01:17:09 +0530

    Update ADS.py

--- ADS.py
+++ ADS.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyMHx/IVW+qkQRmMyTJXc6eC",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
